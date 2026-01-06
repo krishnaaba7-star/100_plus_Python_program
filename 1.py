ef3f850c5b1c4b1ffee2a459079fc9b574b9a3b5
Solanki Krishnaba <krishnaaba7star@gmail.com>
2026-01-06 20:24:26 +0530

    Added Python programs

---
+++ 1.py
@@ -0,0 +1,6 @@
+#Python Program to Add Two Numbers
+
+num1 = 30
+num2 = 40
+sum = num1+num2
+print("the sum of givien two number is",sum)
---
+++ 1_1.py
@@ -0,0 +1,7 @@
+#Python Program to Add Two Numbers
+num1 = float(input("Enter a number here:"))
+num2 = float(input("Enter Another number here:"))
+
+sum = num1 + num2
+
+print("the sum of the provided two numbers is ",sum)
