bb22f793353a9d82a5dabc7b7628e2fab0364c7f
Solanki Krishnaba <krishnaaba7star@gmail.com>
2026-01-19 20:49:22 +0530

    Add initial code

---
+++ 7.py
@@ -0,0 +1,10 @@
+# python program to check if a number is positive , megative or 0
+# using conditional statements
+
+num = 12
+if num > 0:
+    print("It is a positive number")
+elif num == 0:
+    print("it is zero")
+else:
+    print("it is a negative number")
---
+++ 7_1.py
@@ -0,0 +1,7 @@
+num = int(input("enter a number here"))
+if num > 0:
+    print("It is a positive number")
+elif num == 0:
+    print("it is zero")
+else:
+    print("it is a negative number")
