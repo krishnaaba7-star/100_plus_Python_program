0c5ba2c92c1949bba8ef16d3dd4293ad8fa08221
krishnaaba7-star <krishnaaba7@gmail.com>
2026-02-09 20:18:45 +0530

    8_1.py

---
+++ 8_1.py
@@ -0,0 +1,7 @@
+#Python program to check if a number is odd or even 
+num = int(input("enter a number"))
+
+if num % 2 == 0:
+    print("It is an even number")
+else:
+    print("it is an odd number")
