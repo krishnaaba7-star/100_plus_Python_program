d8f98fe77d77598f7b34ccfb31088ca71c29c49e
krishnaaba7-star <krishnaaba7@gmail.com>
2026-02-16 21:22:38 +0530

    python program to check Prime Numbers

---
+++ 11.py
@@ -0,0 +1,11 @@
+# python program to check Prime Numbers
+num = int(input("enter a number here:"))
+if num == 1:
+    print("it is not a prime number")
+if num > 1 :
+    for i in range (2,num):
+        if num % i == 0:
+            print("it is not a prime number")
+            break 
+    else :
+        print("it is a prime number")
